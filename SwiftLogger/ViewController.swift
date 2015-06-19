379d325c78e16b2b06acd9ac61339a9ea6545052
Mugunth <mugunth.kumar@gmail.com>
2015-06-19 13:00:06 +0800

    Initial Commit

---
+++ SwiftLogger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftLogger
+//
+//  Created by Mugunth Kumar on 19/6/15.
+//  Copyright (c) 2015 Steinlogic Consulting and Training Lte Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
